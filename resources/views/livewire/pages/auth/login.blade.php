30fc0e968adf50e27d449569b296ca520f6d0ccd
CodeWithRibo <riboluna2818@gmail.com>
2025-11-19 17:30:54 +0800

    refactor: modify design

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
-{
+new #[Layout('layouts.guest')]
+class extends Component {
     public LoginForm $form;
 
     /**
@@ -24,48 +24,80 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<div class="grid grid-cols-1 2xl:grid-cols-2 relative">
+        <div class=" hidden 2xl:block bg-gradient-to-br from-[#2E7D32] to-[#66BB6A] min-h-screen">
+            <div class="p-32 flex flex-col items-center justify-center space-y-5">
+                 <span class=" rounded-xl p-[10px] bg-[#2E7D32]">
+                      <x-ui.icon name="ps:leaf" variant="bold" class="size-6   "/>
+                 </span>
+                <h1 class="text-white mb-6 text-5xl font-bold">Welcome to EcoMart</h1>
+                <p class="text-white text-xl">Your destination for fresh, organic groceries.</p>
 
-    <form wire:submit="login">
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
+                <img src="{{asset('login-fruit-img.jpg')}}"
+                     alt="login-fruit"
+                     class="w-full h-full object-cover object-center rounded-3xl">
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
+    <div class="bg-white flex flex-col items-center min-h-screen justify-center ">
+        <div class=" flex 2xl:hidden items-center gap-3 pb-2">
+                       <span class="rounded-xl p-[10px] bg-[#2E7D32]">
+                           <x-ui.icon name="ps:leaf" variant="bold" class="size-6 "/>
+                       </span>
+            <h1 class="text-2xl text-[#2E7D32]">EcoMart</h1>
         </div>
+        <fieldset class="shadow-lg rounded-xl p-10">
+            <h1 class="text-gray-900 text-2xl">Welcome Back</h1>
+            <p class="text-gray-500 text-base">Sign in to your account to continue</p>
+            <form wire:submit="login">
+                <!-- Email Address -->
+                <div class="pt-5">
+                    <x-input-label for="email" :value="__('Email')"/>
+                    <x-text-input wire:model="form.email" id="email"
+                                  class="block mt-1 w-full rounded-full focus:border-green-500 focus:ring-green-500"
+                                  type="email"
+                                  name="email"
+                                  placeholder="your.email@example.com"
+                                  required autofocus autocomplete="username"/>
+                    <x-input-error :messages="$errors->get('form.email')" class="mt-2"/>
+                </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember" class="inline-flex items-center">
-                <input wire:model="form.remember" id="remember" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('Password')"/>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}" wire:navigate>
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+                    <x-text-input wire:model="form.password" id="password"
+                                  class="block mt-1 w-full focus:border-green-500 focus:ring-green-500"
+                                  type="password"
+                                  name="password"
+                                  placeholder="Enter your password"
+                                  required autocomplete="current-password"/>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+                    <x-input-error :messages="$errors->get('form.password')" class="mt-2"/>
+                </div>
+
+                <div class="mt-4 flex items-center justify-end">
+
+                    @if (Route::has('password.request'))
+                        <a class="text-sm text-[#2E7D32] hover:text-[#66BB6A]  rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                           href="{{ route('password.request') }}" wire:navigate>
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                </div>
+
+                <div class=" mt-4">
+                    <x-ui.button type="submit" class="w-full bg-green-700 text-white py-3.5 rounded-xl hover:bg-green-600 focus:bg-green-600 transition-colors text-center">Sign In</x-ui.button>
+                </div>
+
+                <div class="mt-4 text-center">
+                    <p class="text-gray-500 text-sm">
+                        Don't have an account?
+                        <a href="{{ route('register') }}"
+                           class="text-[#2E7D32] hover:text-[#66BB6A] text-sm">Sign up for free</a>
+                    </p>
+                </div>
+            </form>
+        </fieldset>
+    </div>
+
 </div>
--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -36,53 +36,82 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
-    <form wire:submit="register">
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<div class="grid grid-cols-1 2xl:grid-cols-2 relative">
+    <div class=" hidden 2xl:block bg-gradient-to-br from-[#2E7D32] to-[#66BB6A] min-h-screen">
+        <div class="p-32 flex flex-col items-center justify-center space-y-5">
+                 <span class=" rounded-xl p-[10px] bg-[#2E7D32]">
+                      <x-ui.icon name="ps:leaf" variant="bold" class="size-6   "/>
+                 </span>
+            <h1 class="text-white mb-6 text-5xl font-bold">Welcome to EcoMart</h1>
+            <p class="text-white text-xl">Your destination for fresh, organic groceries.</p>
+
+            <img src="{{asset('login-fruit-img.jpg')}}"
+                 alt="login-fruit"
+                 class="w-full h-full object-cover object-center rounded-3xl">
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+    <div class="bg-white flex flex-col items-center min-h-screen justify-center ">
+        <div class=" flex 2xl:hidden items-center gap-3 pb-2">
+                       <span class="rounded-xl p-[10px] bg-[#2E7D32]">
+                           <x-ui.icon name="ps:leaf" variant="bold" class="size-6 "/>
+                       </span>
+            <h1 class="text-2xl text-[#2E7D32]">EcoMart</h1>
         </div>
+        <fieldset class="shadow-lg rounded-xl p-10">
+            <h1 class="text-gray-900 text-2xl">Welcome Back</h1>
+            <p class="text-gray-500 text-base">Register in to your account to continue</p>
+            <form wire:submit="register">
+                <!-- Name -->
+                <div class="mt-2">
+                    <x-input-label for="name" :value="__('Name')" />
+                    <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
 
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('Password')" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                    <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
+                                  type="password"
+                                  name="password"
+                                  required autocomplete="new-password" />
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
 
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                <!-- Confirm Password -->
+                <div class="mt-4">
+                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                    <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
+                                  type="password"
+                                  name="password_confirmation" required autocomplete="new-password" />
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
-                {{ __('Already registered?') }}
-            </a>
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+                <div class="mt-4 flex items-center justify-end">
+
+                    <a class="text-sm text-[#2E7D32] hover:text-[#66BB6A]  rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
+                        {{ __('Already registered?') }}
+                    </a>
+                </div>
+
+                <div class=" mt-4">
+                    <x-ui.button type="submit" class="w-full bg-green-700 text-white py-3.5 rounded-xl hover:bg-green-600 focus:bg-green-600 transition-colors text-center">Register</x-ui.button>
+                </div>
+
+            </form>
+        </fieldset>
+    </div>
+
 </div>
+
