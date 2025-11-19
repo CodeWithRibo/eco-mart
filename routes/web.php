f69810dfa6f21fbfd50bdc662981ed413f81c3e7
CodeWithRibo <riboluna2818@gmail.com>
2025-11-19 18:01:55 +0800

    chore: role based & middleware

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,29 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+/*Routing for User*/
+Route::middleware(['auth', 'verified', 'is_user'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('profile', 'profile')->name('profile');
+});
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+/*Routing for Admin*/
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::get('admin/dashboard', function () {
+        return 'admin dashboard';
+    });
+});
+
+Route::get('logout', function(Request $request) {
+    auth()->guard('web')->logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+
+    return redirect()->route('login');
+});
 
 require __DIR__.'/auth.php';
